358e73e803318cc14344d9cf4a3111fe14b40c6a
Piratb707 <beloshapkobm@yandex.ru>
2023-03-09 22:32:07 -0300

    add to git

---
+++ project/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    with open('project/file.txt','r',encoding='utf=8') as file:
+        resultData=list()
+        for line in file.readlines():
+            resultData.append(tuple(line.split('\n')[0].split(';')))
+
+    return render_template('base.html', data=resultData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run()
