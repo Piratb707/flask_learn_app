62a5e3f9d67ff1b36a69e161b634186894158410
Piratb707 <beloshapkobm@yandex.ru>
2023-03-09 23:54:35 -0300

    add some feature

--- project/main.py
+++ project/main.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
+from LoginForm import Lf
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello hello hello world'
+
 
 @app.route('/')
 def main():
-    with open('project/file.txt','r',encoding='utf=8') as file:
-        resultData=list()
-        for line in file.readlines():
-            resultData.append(tuple(line.split('\n')[0].split(';')))
+    return render_template('base.html')
 
-    return render_template('base.html', data=resultData)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/register', method=['GET','POST'])
+def register():
+    form = Lf()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 if __name__ == '__main__':
     app.run()
