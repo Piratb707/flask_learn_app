65390705a663037aea1c0fe4a4e96325bde185d9
Piratb707 <beloshapkobm@yandex.ru>
2023-03-10 01:51:35 -0300

    Finally reg+Auth

--- project/loginForm.py
+++ project/loginForm.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-fro, wtforms.validators import DataRequired
+from wtforms.validators import DataRequired
 
 class Lf(FlaskForm):
     name = StringField("Имя", validators=[DataRequired()]
